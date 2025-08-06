058cdc3aa3850a1d17fbb76c9a0f5f254672e0cf
Sarah Covert <sarah.e.belden@gmail.com>
2025-08-06 14:06:24 -0500

    Initial Flask app with vehicle list

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,25 @@
-# app/routes.py
-
 from flask import Blueprint, render_template
-from .models import Vehicle
 
 main = Blueprint('main', __name__)
 
-# Dummy vehicle data
+# temporary dummy data
+class Vehicle:
+    def __init__(self, id, make, model, year, vin):
+        self.id = id
+        self.make = make
+        self.model = model
+        self.year = year
+        self.vin = vin
+
 vehicles = [
     Vehicle(1, "Ford", "F-150", 2022, "1FTFW1E50NFA00001"),
     Vehicle(2, "Chevy", "Silverado", 2021, "3GCNWAEF4MG100002"),
-    Vehicle(3, "Dodge", "Durango", 2020, "1C4RDJDG0LC300003")
+    Vehicle(3, "Dodge", "Durango", 2020, "1C4RDJDG0LC300003"),
 ]
 
 @main.route('/')
 def home():
-    return "Hello, Sarah! Your Flask app is running 💥"
-
-@main.route('/vehicles')
-def vehicle_list():
     return render_template('vehicles.html', vehicles=vehicles)
+
 
 
